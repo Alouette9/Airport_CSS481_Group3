0d7f99dc5ca638dd5eb58dc1dfe8820aa97fa4d0
jyeung29 <jyeung9@gmail.com>
2025-10-15 18:37:20 -0700

    Add Default index.html with shared consts in app.js

---
+++ src/app.js
@@ -0,0 +1,11 @@
+//Shared html elements
+const dataTitle = document.getElementById("dataTitle");
+const dataDisplay = document.getElementById("dataDisplay");
+const viewSelect = document.getElementById("viewSelect");
+
+//Place your code in your sections
+//Larry
+
+//Jasper
+
+//Pamela
